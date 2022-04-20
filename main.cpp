9a3a687d4c8db230b0d2de89d72d2dbe46db6718
Tata_kn18 <karmikday@gmail.com>
2022-04-20 20:49:56 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 int main()
 {
+	int size;
+	cout << "Enter the size->";
+	cin >> size;
+	int* arr = new int[size];
+	cout << "Enter the values->" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+	while (true)
+	{
+		int choice;
+		cout << "\nMenu:" << endl;	
+		cout << "1 - Remove even values ​​from an array";
+		cout << "2 - Remove odd values ​​from an array";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+		{
+			int ns = 0;
+			for (int i = 0; i < size; i++)
+			{
+				if (arr[i] % 2 != 0)
+				{
+					arr[ns++] = arr[i];
+				}
+			}
+			for (int i = 0; i < ns; i++) 
+			{
+				cout << arr[i] << " ";
+			}
+		}break;
+		case 2:
+		{
 
+
+
+
+		}break;
+		}
+	}
 }

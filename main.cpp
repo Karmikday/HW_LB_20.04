7a22cb7d0b63104b5a8ffee8fcade309943c4574
Tata_kn18 <karmikday@gmail.com>
2022-04-25 17:22:45 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -28,6 +28,67 @@ int main()
 		cout << "------------------------------------" << endl;
 		cout << "FINAL ARRAY" << endl;
 		cout << "------------------------------------" << endl;
-		int* arr3 = new int[size];
+        int tmp = 0;
+        for (int i = 0; i < size; i++)
+        {
+            bool inBoth = false;
+            for (int j = 0; j < size; j++)
+            {
+                if (arr1[i] == arr2[j]) {
+                    inBoth = true;
+                    break;
+                }
+            }
+            if (!inBoth)
+                tmp++;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            bool inBoth = false;
+            for (int j = 0; j < size; j++)
+            {
+                if (arr2[i] == arr1[j]) {
+                    inBoth = true;
+                    break;
+                }
+            }
+            if (!inBoth)
+                tmp++;
+        }
+
+        int size3 = tmp;
+        int* arr3 = new int[size3];
+        tmp = 0;
+        for (int i = 0; i < size; i++)
+        {
+            bool inBoth = false;
+            for (int j = 0; j < size; j++)
+            {
+                if (arr1[i] == arr2[j]) {
+                    inBoth = true;
+                    break;
+                }
+            }
+            if (!inBoth)
+                arr3[tmp++] = arr1[i];
+        }
+        for (int i = 0; i < size; i++)
+        {
+            bool inBoth = false;
+            for (int j = 0; j < size; j++)
+            {
+                if (arr2[i] == arr1[j]) {
+                    inBoth = true;
+                    break;
+                }
+            }
+            if (!inBoth)
+                arr3[tmp++] = arr2[i];
+        }
+        for (int i = 0; i < size3; i++)
+            cout << arr3[i] << " ";
+
+        cout << endl;
+
 		cout << "------------------------------------" << endl;
 	}

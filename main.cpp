4b70323780bd6865172089944fd02066980c5aa7
Tata_kn18 <karmikday@gmail.com>
2022-04-25 17:25:13 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
+void mas1() {
+	const int size1 = 5;
+	int A[size1] = { 1,2,3,4,5 };
+	int* pa = A;
+
+	for (int i = 0; i < size1; i++)
+		cout << *pa++ << ' ';
+	cout << '\n';
+
+}
+void mas2() {
+	const int size2 = 6;
+	int B[size2] = { 6,4,2,8,9,7 };
+	int* pb = B;
+
+	for (int i = 0; i < size2; i++)
+		cout << *pb++ << ' ';
+	cout << '\n';
+}
+
+void mas() {
+	const int size = 11;
+	int C[size] = { 1,2,3,4,5,6,4,2,8,9,7 };
+	int* pc = C;
+
+	for (int i = 0; i < size; i++)
+		cout << *pc++ << ' ';
+	cout << '\n';
+}
+
+int main() 
 {
-	
+	mas();
+	mas2();
+	mas1();
+
 }
